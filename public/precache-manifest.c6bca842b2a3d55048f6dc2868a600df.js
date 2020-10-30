67b0ff8e63abf26721ae264731fa1bd6f50ccac8
Ivan Solnyshkin <ethosy@yandex.ru>
2020-10-30 12:22:35 +0700

    feat: init + static

---
+++ public/precache-manifest.c6bca842b2a3d55048f6dc2868a600df.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "39f1c7a528f954a2b2dd8b5766a47072",
+    url: "/index.html",
+  },
+  {
+    revision: "16cb7b0185f0237f830e",
+    url: "/static/css/main.10b914b0.chunk.css",
+  },
+  {
+    revision: "87fe720923abf27d7cd4",
+    url: "/static/js/2.6e4763c2.chunk.js",
+  },
+  {
+    revision: "e88a3e95b5364d46e95b35ae8c0dc27d",
+    url: "/static/js/2.6e4763c2.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "16cb7b0185f0237f830e",
+    url: "/static/js/main.8b9c61a3.chunk.js",
+  },
+  {
+    revision: "6e04319a44e5e6859875",
+    url: "/static/js/runtime-main.36fb0e45.js",
+  },
+  {
+    revision: "09f4068bb74fc78ffbce74f5596d6aa5",
+    url: "/static/media/Inter-Black.09f4068b.woff2",
+  },
+  {
+    revision: "e37354838bf8078ac0d296217613533f",
+    url: "/static/media/Inter-Black.e3735483.woff",
+  },
+  {
+    revision: "7a8cc7241f766a142e15b2948804e547",
+    url: "/static/media/Inter-Medium.7a8cc724.woff",
+  },
+  {
+    revision: "85f4b8e903fbd1d2c98a67dd0dca42e7",
+    url: "/static/media/Inter-Medium.85f4b8e9.woff2",
+  },
+  {
+    revision: "4dd66a113d54a7f9a1ae913049610617",
+    url: "/static/media/Inter-Regular.4dd66a11.woff2",
+  },
+  {
+    revision: "7c539936c4c8c822b59a1bcc6c08a6ec",
+    url: "/static/media/Inter-Regular.7c539936.woff",
+  },
+  {
+    revision: "3c82ae069680d348b272ed17ca28b3d6",
+    url: "/static/media/close-icon.3c82ae06.svg",
+  },
+  {
+    revision: "b05083e51f8e21ef50c2b78b29bb740c",
+    url: "/static/media/edit-btn.b05083e5.svg",
+  },
+  {
+    revision: "b2fce4ac45f3d116cd48ec8d068ab723",
+    url: "/static/media/krest.b2fce4ac.svg",
+  },
+  {
+    revision: "5bbb05f725aa334a73acf3174e255f4e",
+    url: "/static/media/like-active.5bbb05f7.svg",
+  },
+  {
+    revision: "a8acc871456b1001597672e02c5199ca",
+    url: "/static/media/like.a8acc871.svg",
+  },
+  {
+    revision: "d476a8b86d0b656cfa463c1148e96069",
+    url: "/static/media/profile-edit-hover.d476a8b8.svg",
+  },
+  {
+    revision: "d80783439d382b46ce24b8da369767d4",
+    url: "/static/media/trash.d8078343.svg",
+  },
+  {
+    revision: "c6f110191388042ab958ac5e9b285425",
+    url: "/static/media/vector.c6f11019.svg",
+  },
+]);
