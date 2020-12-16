d0eec15d661da677a430f5031aba0d20a5fae48e
Ivan Solnyshkin <ethosy@yandex.ru>
2020-12-16 21:57:11 +0700

    autoriz

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,9 @@
 const jwt = require('jsonwebtoken');
 
-const auth = (req, res, next) => {
+module.exports = autoriz = (req, res, next) => {
   const { authorization } = req.headers;
-  console.log(authorization, ' авторизация');
-  console.log(!authorization || !authorization.startsWith('Bearer '), ' avto ',
-    (!authorization), ' start ', !authorization.startsWith('Bearer '));
+
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    console.log('Ошибка 1');
     return res.status(400).send({ message: 'Нужно авторизироваться' });
   }
   const token = authorization.replace('Bearer ', '');
@@ -15,12 +12,9 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'key-secret');
   } catch (err) {
-    console.log('Ошибка 2');
     return res.status(400).send({ message: 'Нет атворизации' });
   }
 
   req.user = payload;
   next();
 };
-
-module.exports = auth;
