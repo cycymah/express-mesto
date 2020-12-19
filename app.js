a3c8e64a3b1de4fef7b9ed83b43dfdcfcd3a180b
Ivan Solnyshkin <ethosy@yandex.ru>
2020-12-19 20:40:05 +0700

    no message

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post('/signup',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required.min(8),
+      password: Joi.string().required().min(8),
     }),
   }),
   createUser);
